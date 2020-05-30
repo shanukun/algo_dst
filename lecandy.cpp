ac9a3ccc22de66f54a584f25104e60426720de5d
Pratham Singh <prathms007@gmail.com>
2020-05-30 17:23:43 +0530

    change

--- lecandy.cpp
+++ lecandy.cpp
@@ -3,16 +3,16 @@
 #include<bits/stdc++.h>
 
 #define pb push_back
-#define rep(a,b,c) for(int a=(int)b;a<(int)c;a++)
-#define repe(a,b,c) for(int a=(int)b;a<=(int)c;a++)
-#define repk(a,b,c,k) for(int a=(int)b;a<(int)c;a+=(int)k)
-#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define rep(a, b, c) for(int a = (int)b; a < (int)c; a++)
+#define repe(a, b, c) for(int a = (int)b; a <= (int)c; a++)
+#define repk(a, b, c, k) for(int a = (int)b; a < (int)c; a += (int)k)
+#define fastio std::ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #define mod 1000000007
 #define modu 998244353
 #define int long long int
-#define pii pair<int,int>
-#define piii pair<int,pair<int,int>>
-#define ful(a) a.begin(),a.end()
+#define pii pair<int, int>
+#define piii pair<int, pair<int, int>>
+#define ful(a) a.begin(), a.end()
 #define ub upper_bound
 #define lb lower_bound
 #define ff first
